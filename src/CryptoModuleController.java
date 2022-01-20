104a54a7a0e7e78bee11aae34862a5229ac440c1
ReTRAI <geral@retrai.co>
2022-01-20 16:56:39 +0000

    Importação BCFipsProvider

--- src/CryptoModuleController.java
+++ src/CryptoModuleController.java
@@ -1,2 +1,10 @@
+import org.bouncycastle.crypto.EntropySourceProvider;
+import org.bouncycastle.crypto.fips.FipsDRBG;
+import org.bouncycastle.crypto.util.BasicEntropySourceProvider;
+
 public class CryptoModuleController {
+
+
+
+
 }
